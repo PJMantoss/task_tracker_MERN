55289950f6ca6297c36ff504e6ddd02f830a8e5b
PJMantoss <PJMantoss>
2025-07-18 18:45:56 +0200

    update server.js

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 
+connectDB();
+
+// Init Middleware
+app.use(express.json());
+app.use(require('cors')());
+
+app.get('/', (req, res) => res.send('API Running'));
