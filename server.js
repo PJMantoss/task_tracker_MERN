47c3d6b6fb5d475bef18b5b2569fa5fe160d1179
PJMantoss <PJMantoss>
2025-07-18 18:46:38 +0200

    update server.js

--- server.js
+++ server.js
@@ -10,3 +10,10 @@ app.use(express.json());
 app.use(require('cors')());
 
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/tasks', require('./routes/api/tasks'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
