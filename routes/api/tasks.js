cb1a204ede88509fbdd9ed69cb870c21aab552f2
PJMantoss <PJMantoss>
2025-07-20 20:31:30 +0200

    update routes/api/tasks

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -2,3 +2,13 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const Task = require('../../models/Task');
+
+//GET api/tasks: Get all user's tasks
+router.get('/', auth, async (req, res) => {
+  try {
+    const tasks = await Task.find({ user: req.user.id }).sort({ date: -1 });
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).send('Server Error');
+  }
+});
