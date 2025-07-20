593a869797a9cff7c308a43cd952ec9efcb2c6ef
PJMantoss <PJMantoss>
2025-07-20 20:32:46 +0200

    update routes/api/tasks

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -26,3 +26,17 @@ router.post('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+//PUT api/tasks/:id - Update a task
+router.put('/:id', auth, async (req, res) => {
+    try {
+        let task = await Task.findById(req.params.id);
+        if (!task) return res.status(404).json({ msg: 'Task not found' });
+        if (task.user.toString() !== req.user.id) return res.status(401).json({ msg: 'Not authorized' });
+        
+        task = await Task.findByIdAndUpdate(req.params.id, { completed: !task.completed }, { new: true });
+        res.json(task);
+    } catch (err) {
+        res.status(500).send('Server Error');
+    }
+});
