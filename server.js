e6a0b83964eb989b65111c825ca6891a11104ab8
PJMantoss <PJMantoss>
2025-08-09 14:35:23 +0200

    update server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config(); // Load environment variables
 const express = require('express');
-const connectDB = require('../config/db');
+const connectDB = require('./config/db');
 const app = express();
 
 connectDB();
@@ -12,8 +12,8 @@ app.use(require('cors')());
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
-app.use('/api/auth', require('../routes/api/auth'));
-app.use('/api/tasks', require('../routes/api/tasks'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/tasks', require('./routes/api/tasks'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
