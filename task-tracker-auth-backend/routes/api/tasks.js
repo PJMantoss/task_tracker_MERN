c44f49dd922ba15c2b2dcd0ddae362e17ec982d3
PJMantoss <PJMantoss>
2025-08-10 11:56:04 +0200

    update backend

---
+++ task-tracker-auth-backend/routes/api/tasks.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const Task = require('../../models/Task');
+
+// @route   GET api/tasks
+// @desc    Get all user's tasks
+router.get('/', auth, async (req, res) => {
+  try {
+    const tasks = await Task.find({ user: req.user.id }).sort({ date: -1 });
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   POST api/tasks
+// @desc    Create a task
+router.post('/', auth, async (req, res) => {
+  try {
+    const newTask = new Task({
+      title: req.body.title,
+      user: req.user.id,
+    });
+    const task = await newTask.save();
+    res.json(task);
+  } catch (err) {
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   PUT api/tasks/:id
+// @desc    Update a task
+router.put('/:id', auth, async (req, res) => {
+    try {
+        let task = await Task.findById(req.params.id);
+        if (!task) return res.status(404).json({ msg: 'Task not found' });
+        if (task.user.toString() !== req.user.id) return res.status(401).json({ msg: 'Not authorized' });
+        
+        task = await Task.findByIdAndUpdate(req.params.id, { completed: !task.completed }, { new: true });
+        res.json(task);
+    } catch (err) {
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   DELETE api/tasks/:id
+// @desc    Delete a task
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    let task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ msg: 'Task not found' });
+    if (task.user.toString() !== req.user.id) return res.status(401).json({ msg: 'Not authorized' });
+
+    await Task.findByIdAndDelete(req.params.id);
+    res.json({ msg: 'Task removed' });
+  } catch (err) {
+    res.status(500).send('Server Error');
+  }
+});
+
+module.exports = router;
